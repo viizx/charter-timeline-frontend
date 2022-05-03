a0bd160e1f33c95c597a66ea5112386d29ca82df
viizx <viceradicaa@gmail.com>
2022-05-03 16:38:46 -0400

    Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import "./App.css";
 import Timeline from "./components/Timeline";
 import ReservationList from "./components/ReservationList";
+import Reservation from "./components/Reservation";
 import Create from "./components/Create";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Timeline />
-      <ReservationList />
-      <Create />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Timeline />
+          </Route>
+          <Route exact path="/reservations">
+            <ReservationList />
+          </Route>
+          <Route exact path="/reservations/create">
+            <Create />
+          </Route>
+          <Route exact path="/reservations/:id">
+            <Reservation />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
