d7cbf1a2e112dbba054c50397d640721e54127ae
viizx <viceradicaa@gmail.com>
2022-05-26 15:49:44 -0400

    rendering Nav only for Admin User

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import Timeline from "./components/Timeline";
 import Dashboard from "./components/Dashboard";
 import Reservation from "./components/Reservation";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import LoginForm from "./components/auth/LoginForm";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  let authObject = JSON.parse(user);
+  console.log(authObject);
+
+  useEffect(() => {
+    let isMounted = true;
+    const auth = async () => {
+      setUser(localStorage.getItem("user"));
+    };
+
+    auth();
+    return () => {
+      isMounted = false;
+      console.log(authObject);
+    };
+  });
+
   return (
     <Router>
       <div className="App">
-        <Navbar />
+        {authObject && authObject.isAdmin && <Navbar />}
         <Switch>
           <Route exact path="/">
             <Timeline />
           </Route>
           <Route exact path="/dashboard">
-            <Dashboard />
+            {authObject && authObject.isAdmin && (
+              <Dashboard user={authObject} />
+            )}
+            {!authObject && <LoginForm />}
           </Route>
           <Route path="/reservations/:id">
             <Reservation />
