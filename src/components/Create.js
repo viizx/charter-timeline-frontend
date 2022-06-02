0451b664aa8d16636a6b27e64e1f961447e3edb8
viizx <viceradicaa@gmail.com>
2022-06-02 18:13:02 -0400

    accordion changes

--- src/components/Create.js
+++ src/components/Create.js
@@ -75,11 +75,11 @@ const Create = ({ user }) => {
       <div className="accordion-item">
         <h2 className="accordion-header" id="headingOne">
           <button
-            className="accordion-button"
+            className="accordion-button collapsed"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#collapseOne"
-            aria-expanded="true"
+            aria-expanded="false"
             aria-controls="collapseOne"
           >
             Create New Reservation
@@ -87,7 +87,7 @@ const Create = ({ user }) => {
         </h2>
         <div
           id="collapseOne"
-          className="accordion-collapse collapse show"
+          className="accordion-collapse collapse"
           aria-labelledby="headingOne"
           data-bs-parent="#accordionExample"
         >
