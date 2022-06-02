d366d1b53ba12746c1df0ccca3309b04dd5c9161
viizx <viceradicaa@gmail.com>
2022-06-02 17:51:22 -0400

    Added locations for request body and auth token for request header

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
-// import "rsuite/dist/rsuite.min.css";
 
-const Create = () => {
+const Create = ({ user }) => {
   const [isPending, setIsPending] = useState(false);
   const [startDate, setStartDate] = useState([null, null]);
   const [endDate, setEndDate] = useState([null, null]);
   const [ship, setShip] = useState("");
   const [reservation, setReservation] = useState("");
+  const [fromLocation, setFromLocation] = useState("");
+  const [toLocation, setToLocation] = useState("");
   var fillColor = "";
   var x = "";
-  var name = "";
 
   var y1 = new Date(startDate).getTime();
   var y2 = new Date(endDate).getTime();
@@ -23,51 +23,50 @@ const Create = () => {
 
   if (ship === "1") {
     x = "Lady Gita";
-    name = "Lady Gita";
   } else if (ship === "2") {
     x = "Ardura";
-    name = "Ardura";
   } else if (ship === "3") {
     x = "Alba";
-    name = "Alba";
   } else if (ship === "4") {
     x = "Slano";
-    name = "Slano";
   } else if (ship === "5") {
     x = "Vito";
-    name = "Vito";
   } else if (ship === "6") {
     x = "Korab";
-    name = "Korab";
   } else if (ship === "7") {
     x = "Agape Rose";
-    name = "Agape Rose";
   } else if (ship === "8") {
     x = "Son De Mar";
-    name = "Son De Mar";
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const input = { x, y, fillColor };
+    const input = { x, y, fillColor, from: fromLocation, to: toLocation };
 
     setIsPending(true);
+    console.log(input);
 
     const response = await fetch(
       "https://port-3000-js-practice-vice889681.codeanyapp.com/api/reservation",
       {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": user,
+        },
         body: JSON.stringify(input),
       }
     );
 
     if (response) {
+      console.log(response);
       setIsPending(false);
       setEndDate("");
       setStartDate("");
       setShip("");
       setReservation("");
+      setFromLocation("");
+      setToLocation("");
     }
   };
 
@@ -93,94 +92,102 @@ const Create = () => {
           data-bs-parent="#accordionExample"
         >
           <div className="accordion-body">
-              <div className="novi-red-head-2">
-                <div className="row">
-                  <div className="col-sm">Vessel</div>
-                  <div className="col-sm">Reservation type</div>
-                  <div className="col-sm">From Date</div>
-                  <div className="col-sm">To Date</div>
+            <div className="novi-red-head-2">
+              <div className="row">
+                <div className="col-sm">Vessel</div>
+                <div className="col-sm">Reservation type</div>
+                <div className="col-sm">From Location</div>
+                <div className="col-sm">From Date</div>
+                <div className="col-sm">To Location</div>
+                <div className="col-sm">To Date</div>
+              </div>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="col-sm">
+                  <select
+                    className="form-select"
+                    aria-label="Default select example"
+                    value={ship}
+                    placeholder="Select vessel"
+                    onChange={(e) => setShip(e.target.value)}
+                  >
+                    <option defaultValue="">Select vessel</option>
+                    <option value="1">Lady Gita</option>
+                    <option value="2">Ardura</option>
+                    <option value="3">Alba</option>
+                    <option value="4">Slano</option>
+                    <option value="5">Vito</option>
+                    <option value="6">Korab</option>
+                    <option value="7">Agape Rose</option>
+                    <option value="8">Son De Mar</option>
+                  </select>
+                </div>
+                <div className="col-sm">
+                  <select
+                    className="form-select"
+                    aria-label="Default select example"
+                    value={reservation}
+                    placeholder="Reservation"
+                    onChange={(e) => setReservation(e.target.value)}
+                  >
+                    <option defaultValue="">Select your option</option>
+                    <option value="0">Booked</option>
+                    <option value="1">Option</option>
+                  </select>
+                </div>
+                <div className="col-sm">
+                  <input
+                    placeholder="From Location"
+                    type="text"
+                    className="form-control"
+                    value={fromLocation}
+                    onChange={(e) => setFromLocation(e.target.value)}
+                  />
+                </div>
+                <div className="col-sm">
+                  <input
+                    placeholder="Selected date"
+                    type="date"
+                    className="form-select"
+                    value={startDate}
+                    onChange={(e) => setStartDate(e.target.value)}
+                  ></input>
+                </div>
+                <div className="col-sm">
+                  <input
+                    placeholder="To Location"
+                    type="text"
+                    className="form-control"
+                    value={toLocation}
+                    onChange={(e) => setToLocation(e.target.value)}
+                  />
+                </div>
+                <div className="col-sm">
+                  <input
+                    placeholder="Selected date"
+                    type="date"
+                    className="form-select"
+                    value={endDate}
+                    onChange={(e) => setEndDate(e.target.value)}
+                  />
                 </div>
               </div>
-              <form onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="col-sm">
-                    <div className="container">
-                      <select
-                        className="form-select"
-                        aria-label="Default select example"
-                        value={ship}
-                        placeholder="Select vessel"
-                        onChange={(e) => setShip(e.target.value)}
-                      >
-                        <option defaultValue="">Select vessel</option>
-                        <option value="1">Lady Gita</option>
-                        <option value="2">Ardura</option>
-                        <option value="3">Alba</option>
-                        <option value="4">Slano</option>
-                        <option value="5">Vito</option>
-                        <option value="6">Korab</option>
-                        <option value="7">Agape Rose</option>
-                        <option value="8">Son De Mar</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="col-sm">
-                    <div className="container">
-                      <select
-                        className="form-select"
-                        aria-label="Default select example"
-                        value={reservation}
-                        placeholder="Reservation"
-                        onChange={(e) => setReservation(e.target.value)}
-                      >
-                        <option defaultValue="">Select your option</option>
-                        <option value="0">Booked</option>
-                        <option value="1">Option</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="col-sm">
-                    <div className="container">
-                      <input
-                        placeholder="Selected date"
-                        type="date"
-                        className="form-select"
-                        value={startDate}
-                        onChange={(e) => setStartDate(e.target.value)}
-                      ></input>
-                    </div>
-                  </div>
-                  <div className="col-sm">
-                    <div className="container">
-                      <input
-                        placeholder="Selected date"
-                        type="date"
-                        className="form-select"
-                        value={endDate}
-                        onChange={(e) => setEndDate(e.target.value)}
-                      />
-                    </div>
-                  </div>
+              <div className="container">
+                <div className="button">
+                  {!isPending && (
+                    <button type="submit" className="btn btn-primary">
+                      Add Reservation
+                    </button>
+                  )}
+                  {isPending && (
+                    <button disabled type="submit" className="btn btn-primary">
+                      Working...
+                    </button>
+                  )}
                 </div>
-                <div className="container">
-                  <div className="button">
-                    {!isPending && (
-                      <button type="submit" className="btn btn-primary">
-                        Add Reservation
-                      </button>
-                    )}
-                    {isPending && (
-                      <button
-                        disabled
-                        type="submit"
-                        className="btn btn-primary"
-                      >
-                        Working...
-                      </button>
-                    )}
-                  </div>
-                </div>
-              </form>
+              </div>
+            </form>
           </div>
         </div>
       </div>
