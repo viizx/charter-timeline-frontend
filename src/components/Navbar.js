e50a5513a7d40927aa6eaf01d48e11988e9e0570
viizx <viceradicaa@gmail.com>
2022-06-08 18:11:52 -0400

    Tailwind navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,20 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="Navbar">
-      <div className="NavItem">
-        <Link to="/">Timeline</Link>
+    <nav className="bg-indigo-600">
+      <div className="mx-auto py-1.5 px-2 sm:px-6 lg:px-6">
+        <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+          <div className="flex space-x-4">
+            <div className="text-gray-50 hover:bg-indigo-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+              <Link to="/">Timeline</Link>
+            </div>
+            <div className="text-gray-50 hover:bg-indigo-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+              <Link to="/dashboard">Dashboard</Link>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="NavItem">
-        <Link to="/dashboard">Dashboard</Link>
-      </div>
-    </div>
+    </nav>
   );
 }
 
