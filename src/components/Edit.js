2bf2b99b9af8e7787a55b1025e705e952b37449b
viizx <viceradicaa@gmail.com>
2022-06-11 16:31:30 -0400

    Edit tailwind

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -11,53 +11,27 @@ const Edit = (props) => {
   const [endDate, setEndDate] = useState([null, null]);
   const [ship, setShip] = useState("");
   const [reservation, setReservation] = useState("");
-  var fillColor = "";
-  var x = "";
-  var name = "";
+  const [fromLocation, setFromLocation] = useState("");
+  const [toLocation, setToLocation] = useState("");
 
   var y1 = new Date(startDate).getTime();
   var y2 = new Date(endDate).getTime();
 
   var y = [y1, y2];
-  if (reservation === "Booked") {
-    fillColor = "#006066";
-  } else {
-    fillColor = "#061137";
-  }
 
-  if (ship === "1") {
-    x = "Lady Gita";
-    name = "Lady Gita";
-  } else if (ship === "2") {
-    x = "Ardura";
-    name = "Ardura";
-  } else if (ship === "3") {
-    x = "Alba";
-    name = "Alba";
-  } else if (ship === "4") {
-    x = "Slano";
-    name = "Slano";
-  } else if (ship === "5") {
-    x = "Vito";
-    name = "Vito";
-  } else if (ship === "6") {
-    x = "Korab";
-    name = "Korab";
-  } else if (ship === "7") {
-    x = "Agape Rose";
-    name = "Agape Rose";
-  } else if (ship === "8") {
-    x = "Son De Mar";
-    name = "Son De Mar";
-  }
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const input = { x, y, fillColor };
+    const input = {
+      x: ship,
+      y,
+      fillColor: reservation,
+      from: fromLocation,
+      to: toLocation,
+    };
 
     setIsPending(true);
 
-    fetch(
+    const response = await fetch(
       "https://port-3000-js-practice-vice889681.codeanyapp.com/api/reservation/" +
         id,
       {
@@ -65,88 +39,149 @@ const Edit = (props) => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(input),
       }
-    )
-      .then((response) => {
-        console.log(response);
-        console.log(input);
-
-        setIsPending(false);
-      })
-      .then(() => {
-        setEndDate("");
-        setStartDate("");
-        setShip("");
-        setReservation("");
-      });
+    );
+    if (response) {
+      console.log(response);
+      setIsPending(false);
+      setEndDate("");
+      setStartDate("");
+      setShip("");
+      setReservation("");
+      setFromLocation("");
+      setToLocation("");
+    }
   };
 
   return (
-    <div className="container">
+    <div className="container mx-auto py-5 sm:px-3 md:px-100 lg:px-100">
       <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="novi-red-edit">
-            <div className="col-sm">
+        <div className="align-content: center px-4">
+          <div className="grid grid-cols-6 gap-6">
+            <div className="col-span-6 sm:col-span-6 py-2">
+              <label
+                for="selectVessel"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Ship
+              </label>
               <select
-                className="form-select"
+                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 aria-label="Default select example"
-                // defaultValue={props.x}
+                id="selectVessel"
                 value={ship}
                 placeholder="Select vessel"
                 onChange={(e) => setShip(e.target.value)}
               >
                 <option defaultValue="">Select vessel</option>
-                <option value="1">Lady Gita</option>
-                <option value="2">Ardura</option>
-                <option value="3">Alba</option>
-                <option value="4">Slano</option>
-                <option value="5">Vito</option>
-                <option value="6">Korab</option>
-                <option value="7">Agape Rose</option>
-                <option value="8">Son De Mar</option>
+                <option value="Lady Gita">Lady Gita</option>
+                <option value="Ardura">Ardura</option>
+                <option value="Alba">Alba</option>
+                <option value="Slano">Slano</option>
+                <option value="Vito">Vito</option>
+                <option value="Korab">Korab</option>
+                <option value="Agape Rose">Agape Rose</option>
+                <option value="Son De Mar">Son De Mar</option>
               </select>
             </div>
-            <div className="col-sm">
+            <div className="col-span-6 sm:col-span-6 py-2">
+              <label
+                for="reservationType"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Reservation Type
+              </label>
               <select
-                className="form-select"
+                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 aria-label="Default select example"
-                // defaultValue={props.fillColor === "#006066" ? "Booked" : "Option"}
                 value={reservation}
-                placeholder="Reservation"
+                id="reservationType"
+                placeholder=""
                 onChange={(e) => setReservation(e.target.value)}
               >
                 <option defaultValue="">Select your option</option>
-                <option value="0">Booked</option>
-                <option value="1">Option</option>
+                <option value="#006066">Booked</option>
+                <option value="#061137">Option</option>
               </select>
             </div>
-            <div className="col-sm">
+            <div className="col-span-6 sm:col-span-3 py-2">
+              <label
+                for="fromLocation"
+                className="block text-sm font-medium text-gray-700"
+              >
+                From Location
+              </label>
               <input
-                placeholder="Selected date"
+                placeholder=""
+                id="fromLocation"
+                type="text"
+                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                value={fromLocation}
+                onChange={(e) => setFromLocation(e.target.value)}
+              />
+            </div>
+            <div className="col-span-6 sm:col-span-3 py-2">
+              <label
+                for="startDate"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Start Date
+              </label>
+              <input
+                placeholder=""
                 type="date"
-                className="form-select"
-                // defaultValue={props.y[0]}
+                id="startDate"
+                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 value={startDate}
                 onChange={(e) => setStartDate(e.target.value)}
               ></input>
             </div>
-            <div className="col-sm">
+            <div className="col-span-6 sm:col-span-3 py-2">
+              <label
+                for="toLocation"
+                className="block text-sm font-medium text-gray-700"
+              >
+                To Location
+              </label>
               <input
-                placeholder="Selected date"
+                placeholder=""
+                id="toLocation"
+                type="text"
+                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                value={toLocation}
+                onChange={(e) => setToLocation(e.target.value)}
+              />
+            </div>
+            <div className="col-span-6 sm:col-span-3 py-2">
+              <label
+                for="toDate"
+                className="block text-sm font-medium text-gray-700"
+              >
+                End Date
+              </label>
+              <input
+                placeholder=""
                 type="date"
-                // defaultValue={props.y[1]}
-                className="form-select"
+                id="toDate"
+                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 value={endDate}
                 onChange={(e) => setEndDate(e.target.value)}
               />
             </div>
-            <div className="col-sm">
+            <div className="col-span-6 sm:col-span-6 py-2 px-4 py-3 bg-gray-50 text-right sm:px-6">
               {!isPending && (
-                <button type="submit" className="btn btn-primary">
-                  Update
+                <button
+                  type="submit"
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Add Reservation
                 </button>
               )}
               {isPending && (
-                <button disabled type="submit" className="btn btn-primary">
+                <button
+                  disabled
+                  type="submit"
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
                   Working...
                 </button>
               )}
