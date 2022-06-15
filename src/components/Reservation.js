8a92c603f1f65b55d6cdacf57ed7c0d1d8571f57
viizx <viceradicaa@gmail.com>
2022-06-15 18:12:53 -0400

    Reservation color scheme

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -65,7 +65,7 @@ const Reservation = () => {
               {!isPending && (
                 <button
                   onClick={handleDelete}
-                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-slate-900 hover:bg-slate-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-800"
                 >
                   Delete Reservation
                 </button>
@@ -73,7 +73,7 @@ const Reservation = () => {
               {isPending && (
                 <button
                   disabled
-                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-slate-900 hover:bg-slate-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-800"
                 >
                   Working...
                 </button>
