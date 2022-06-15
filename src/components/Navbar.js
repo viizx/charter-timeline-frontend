fb1c389d378ba9a9820fb2bae14c7513e5c3dc25
viizx <viceradicaa@gmail.com>
2022-06-15 18:12:35 -0400

    Navbar color scheme, making whole div a Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,20 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="bg-indigo-600">
+    <nav className="bg-slate-900">
       <div className="mx-auto py-1.5 px-2 sm:px-6 lg:px-6">
         <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
           <div className="flex space-x-4">
-            <div className="text-gray-50 hover:bg-indigo-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-              <Link to="/">Timeline</Link>
-            </div>
-            <div className="text-gray-50 hover:bg-indigo-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-              <Link to="/dashboard">Dashboard</Link>
-            </div>
+            <Link to="/">
+              <div className="text-gray-50 hover:bg-slate-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                Timeline
+              </div>
+            </Link>
+            <Link to="/dashboard">
+              <div className="text-gray-50 hover:bg-slate-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                Dashboard
+              </div>
+            </Link>
           </div>
         </div>
       </div>
