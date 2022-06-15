753299a2ae5c7dfd61a339468ff0d45516f0d7ca
viizx <viceradicaa@gmail.com>
2022-06-15 18:12:21 -0400

    Color scheme on edit form

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -171,16 +171,16 @@ const Edit = (props) => {
               {!isPending && (
                 <button
                   type="submit"
-                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-slate-900 hover:bg-slate-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-800"
                 >
-                  Add Reservation
+                  Edit Reservation
                 </button>
               )}
               {isPending && (
                 <button
                   disabled
                   type="submit"
-                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-slate-900 hover:bg-slate-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-800"
                 >
                   Working...
                 </button>
