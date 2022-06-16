1b995ff52e3300df478a7d1f86d8c9b8e9ca90e3
viizx <viceradicaa@gmail.com>
2022-06-16 13:23:54 -0400

    Conditional rendering of Create New Reservation Form

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Read from "./Read";
 import useFetch from "../utility/useFetch";
 import Loading from "./Loading";
@@ -8,11 +9,29 @@ const Dashboard = ({ user }) => {
     "https://port-3000-js-practice-vice889681.codeanyapp.com/api/reservation"
   );
 
+  const [modal, setModal] = useState(false);
+
   return (
     <>
-      <div className="align-content: center">
-        <Create user={user} />
+      <div className="container mx-auto py-5 sm:px-3 md:px-100 lg:px-100">
+        {!modal && (
+          <button
+            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-slate-900 hover:bg-slate-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-800"
+            onClick={() => setModal(!modal)}
+          >
+            Create new Reservation
+          </button>
+        )}
+        {modal && (
+          <button
+            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-slate-900 hover:bg-slate-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-800"
+            onClick={() => setModal(!modal)}
+          >
+            Hide
+          </button>
+        )}
       </div>
+      {modal && <Create user={user} />}
       {isPending && <Loading />}
       {reservations && <Read reservations={reservations} />}
     </>
