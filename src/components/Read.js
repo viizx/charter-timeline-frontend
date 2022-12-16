3fbd298a0f5b8032643048989ab8dd11bd036f73
viceradica <vradica_ag@medable.com>
2022-12-16 17:14:32 +0100

    Filter by ship name and time

--- src/components/Read.js
+++ src/components/Read.js
@@ -1,18 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Read = ({ reservations }) => {
+  console.log(reservations)
   const options = { year: 'numeric', month: 'short', day: 'numeric' }
+  const [searchTerm, setSearchTerm] = useState('')
+  const [searchFromDate, setSearchFromDate] = useState('')
+  const [searchToDate, setSearchToDate] = useState('')
 
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const handleFromDateChange = (event) => {
+    setSearchFromDate(event.target.value)
+  }
+
+  const handleToDateChange = (event) => {
+    setSearchToDate(event.target.value)
+  }
+
+  const filterReservations = (reservation) => {
+    if (!searchFromDate || !searchToDate) return reservation.x.toLowerCase().includes(searchTerm.toLowerCase())
+    return reservation.x.toLowerCase().includes(searchTerm.toLowerCase()) &&
+    ((reservation.y[0]) >= new Date(searchFromDate).getTime() && (reservation.y[1]) <= new Date(searchToDate).getTime())
+  }
+  const filteredReservations = reservations.filter(filterReservations)
   return (
     <div className="text-center min-h-full items-center justify-center py-12  sm:px-4 md:px-6 lg:px-8">
+      <div className="flex align-center flex-wrap py-2">
+          Search by Ship name: <input
+            className="mt-1 block py-2 w-full border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-slate-800 focus:border-slate-800 sm:text-sm"
+            type="text"
+            id="search"
+            value={searchTerm}
+            onChange={handleChange}
+            placeholder="Search by Ship name"
+          />
+          Date From Filter:<input
+            className="mt-1 block py-2 w-full border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-slate-800 focus:border-slate-800 sm:text-sm"
+            type="date"
+            id="from-date"
+            value={searchFromDate}
+            onChange={handleFromDateChange}
+            placeholder="From"
+          />
+          Date To Filter:<input
+            className="mt-1 block py-2 w-full border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-slate-800 focus:border-slate-800 sm:text-sm"
+            type="date"
+            id="to-date"
+            value={searchToDate}
+            onChange={handleToDateChange}
+            placeholder="To"
+          />
+      </div>
       <div className="grid grid-cols-3 gap-6">
         <div className="col-span-1 sm:col-span-1 py-2">Ship</div>
         <div className="col-span-1 sm:col-span-1 py-2">From</div>
         <div className="col-span-1 sm:col-span-1 py-2">To</div>
       </div>
 
-      {reservations.map((reservation) => (
+      {filteredReservations.map((reservation) => (
         <div key={reservation._id}>
           <Link to={`reservations/${reservation._id}`}>
             <div className="grid grid-cols-3 gap-6 hover:bg-gray-200">
